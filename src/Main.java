bdb44abbcf95ea97eea53a169d65a8f6f7bdb5f7
Clément Rault <clem.rault@outlook.com>
2022-09-20 12:13:40 -0400

    Modification des fonctions de recherche et du point d'entrée

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,35 @@ import laboratoire1.RechercheTableau;
 
 public class Main {
 
-    public static void main(String[] args) {
-        RechercheTableau rt = new RechercheTableau();
-        int N = 100000000;
-        int[] tab = new int[N];
-        for (int i = 0; i < tab.length; i++) {
-            tab[i] = i;
-        }
-        System.out.println(rt.RechercheLineaire(new int[]{1, 2, 3, 4, 5}, 5, 5));
-        System.out.println(rt.RechercheBinaire(tab, N, N/4+10));
-        int random = (int) (Math.random() * N);
-        System.out.println(rt.RechercheBinaireModifie(tab, N, random) + " r =" + random);
-    }
+    public static void main(String[] args){
+        RechercheTableau searcher = new RechercheTableau();
+        int[] tabLongueur = new int[] {1, 10, 100, 1000, 10000, 100000, 1000000}; // Liste des longueurs de tableaux successives
+        for (int i=0; i<tabLongueur.length; i++){
+            int longueur = tabLongueur[i]; // Longueur de la liste en cours
+            int[] tab = new int[longueur];
+            for (int j=0; j<longueur; j++){
+                tab[j] = 2 * j;
+            }
+            int value = tab[(int)(Math.random() * longueur - 1)];
 
+            long startTime = System.nanoTime();
+            int linear = searcher.RechercheLineaire(tab, tab.length, value);
+            long endTime = System.nanoTime();
+
+            long duration = (endTime - startTime);  //diviser par 1000000 pour avoir des millisecondes.
+            System.out.println(duration);
+        }
+
+        /*
+        int linear = searcher.RechercheLineaire(tab, tab.length, 11);
+        int binary = searcher.RechercheBinaire(tab, tab.length, 11);
+        int binaryMod = searcher.RechercheBinaireModifie(tab, tab.length, 11);
+
+
+        System.out.println(linear);
+        System.out.println(binary);
+        System.out.println(binaryMod);
+
+         */
+    }
 }
--- src/laboratoire1/RechercheTableau.java
+++ src/laboratoire1/RechercheTableau.java
@@ -1,57 +1,66 @@
 package laboratoire1;
 
-import java.util.Arrays;
-
 public class RechercheTableau {
 
-    // Ne pas changer ces fonctions, elles seront utilisées pour tester votre programme.
-    // Elles peuvent cependant servir seulement d'interface et utiliser une méthode "helper"
-    // avec des paramètres supplémentaires, au besoin.
-    public int RechercheLineaire(int[] tab, int n, int x) {
-        for (int i = 0; i < n; i++) {
-            if (tab[i] == x) {
+    public int RechercheLineaire(int[] tab, int n, int x){
+        for (int i=0; i < n; i++){
+            if (tab[i] == x){
                 return i;
             }
         }
         return -1;
     }
 
-    public int RechercheBinaire(int[] tab, int n, int val) {
-        if (n == 1)
-            return tab[0] == val ? 0 : -1;
-        int mid = n / 2;
-        if (tab[mid] == val)
-            return mid;
-        if (tab[mid] > val) {
-            return RechercheBinaire(tab, mid, val);
-        } else {
-            int res = RechercheBinaire(Arrays.copyOfRange(tab, mid, n), n - mid, val);
-            return res == -1 ? -1 : mid + res;
-        }
+    public int RechercheBinaire(int[] tab, int n, int val){
+        return HelperBinaire(tab, 0, n - 1, val);
     }
 
-    public int RechercheBinaireModifie(int[] tab, int n, int val) {
-        if (n == 1)
-            return tab[0] == val ? 0 : -1;
-        int gauche = n / 3;
-        if (tab[gauche] == val)
-            return gauche;
+    public int RechercheBinaireModifie(int[] tab, int n, int val){
+        return HelperBinaireModifie(tab, 0, n - 1, val);
+    }
 
-        if (tab[gauche] > val) {
-            return RechercheBinaireModifie(tab, gauche, val);
-        } else {
-            int droite = n / 3 * 2;
-            if (tab[droite] == val)
-                return droite;
-
-            if (tab[droite] > val) {
-                int res = RechercheBinaireModifie(Arrays.copyOfRange(tab, gauche, droite), droite - gauche, val);
-                return res == -1 ? -1 : gauche + res;
-            } else {
-                int res = RechercheBinaireModifie(Arrays.copyOfRange(tab, droite, n), n - droite, val);
-                return res == -1 ? -1 : droite + res;
+    private int HelperBinaire(int[] tab, int firstIndex, int lastIndex, int val){
+        int n = lastIndex - firstIndex + 1;
+        if (n == 1){
+            if (tab[lastIndex] == val){
+                return lastIndex;
             }
+            return -1;
         }
+        int middle = firstIndex + n / 2;
+        if (tab[middle] == val){
+            return middle;
+        }
+
+        if (tab[middle] > val){
+            return HelperBinaire(tab, firstIndex, middle - 1, val);
+        }
+        return HelperBinaire(tab, middle + 1, lastIndex, val);
+    }
+
+    private int HelperBinaireModifie(int[] tab, int firstIndex, int lastIndex, int val){
+        int n = lastIndex - firstIndex + 1;
+        if (n == 1){
+            if (tab[lastIndex] == val){
+                return lastIndex;
+            }
+            return -1;
+        }
+        int gauche = firstIndex + n / 3;
+        int droite = lastIndex - n / 3;
+        if (tab[gauche] == val){
+            return gauche;
+        }
+        if (tab[gauche] > val){
+            return HelperBinaireModifie(tab, firstIndex, gauche - 1, val);
+        }
+        if (tab[droite] == val){
+            return droite;
+        }
+        if (tab[droite] < val){
+            return HelperBinaireModifie(tab, droite + 1, lastIndex, val);
+        }
+        return HelperBinaireModifie(tab, gauche - 1, droite + 1, val);
     }
 
 }
