9259a34a3c8d85b04b7cabbf71cb111242aab479
Clément Rault <clem.rault@outlook.com>
2022-09-28 19:13:50 -0400

    Ajout package laboratoire 2 et squelette initial

---
+++ src/laboratoire2/Huffman.java
@@ -0,0 +1,14 @@
+package laboratoire2;
+
+public class Huffman{
+
+    // Ne pas changer ces fonctions, elles seront utilisées pour tester votre programme
+    public void Compresser(String nomFichierEntre, String nomFichierSortie){
+
+    }
+
+    public void Decompresser(String nomFichierEntre, String nomFichierSortie){
+
+    }
+
+}
